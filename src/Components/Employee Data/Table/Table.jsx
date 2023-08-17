eea913bbe60a4b8994073f1b217279e8f443982d
patilpraveen26 <patilpraveen1432@gmail.com>
2023-08-17 12:09:22 +0530

    #

--- src/Components/Employee Data/Table/Table.jsx
+++ src/Components/Employee Data/Table/Table.jsx
@@ -12,7 +12,7 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
-import { ToastContainer,toast } from "react-toastify";
+import { ToastContainer,toast} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 
